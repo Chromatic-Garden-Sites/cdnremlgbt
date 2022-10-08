c6a85a4256521743dbaf188af2c0ed9bbf649382
Aubrey Rose <aubrey@aubrey.lgbt>
2022-10-08 14:32:15 -0400

    Update factorio-server.sh

--- factorio-server.sh
+++ factorio-server.sh
@@ -9,8 +9,9 @@ apt install net-tools -y
 apt install mtr -y
 apt install curl -y
 apt install wget -y
-apt install ssh
-apt install unzip
+apt install ssh -y
+apt install unzip -y
+apt install python3-pip -y
 cd /root/
 wget https://cdn.ram.lgbt/autoconf/files/authorized_keys.ram
 wget https://cdn.ram.lgbt/autoconf/files/sshconfig.ram
@@ -25,4 +26,24 @@ useradd -c "gamemaster" -m gamemaster
 echo "gamemaster:$RANDOM" | chpasswd
 sleep 5
 cd /home/gamemaster
-wget --content-disposition https://www.factorio.com/get-download/latest/headless/linux64
+wget --content-disposition https://www.factorio.com/get-download/latest/headless/linux64 -O factorio.tar.xz
+tar xf factorio.tar.xz
+cd factorio/data/
+cp server-settings.example.json server-settings.json
+nano server-settings.json
+pip install requests
+cd /home/gamemaster/factorio/bin/x64
+wget --content-disposition https://raw.githubusercontent.com/narc0tiq/factorio-updater/master/update_factorio.py
+chmod +x ./update_factorio.py
+cd /home/gamemaster/factorio/bin/x64
+./update_factorio.py -Dxa ./factorio
+mkdir /home/gamemaster/factorio/saves
+/home/gamemaster/factorio/bin/x64/factorio --create /home/gamemaster/factorio/saves/initial.zip
+chown gamemaster:gamemaster /home/gamemaster -R
+cd /etc/systemd/system
+wget https://cdn.ram.lgbt/autoconf/files/factorio-service.ram
+mv factorio-service.ram factorio.service
+systemctl daemon-reload
+systemctl enable factorio
+systemctl start factorio
+echo Done
