c2393cedb79dc7c7351f504f8e91e72cc80e734d
Aubrey Rose <aubrey@aubrey.lgbt>
2022-10-08 03:30:05 -0400

    Build MOTD on setup

--- init.sh
+++ init.sh
@@ -11,8 +11,10 @@ mkdir /root/.ssh
 cd /root/
 wget https://cdn.ram.lgbt/autoconf/files/authorized_keys.ram
 wget https://cdn.ram.lgbt/autoconf/files/sshconfig.ram
+wget https://cdn.ram.lgbt/autoconf/files/issue.net.ram
 mv /root/authorized_keys.ram /root/.ssh/authorized_keys
 rm /etc/ssh/sshd_config
 mv /root/sshconfig.ram /etc/ssh/sshd_config
+mv /root/issue.net.ram /etc/issue.net
 systemctl restart ssh
 systemctl restart sshd
