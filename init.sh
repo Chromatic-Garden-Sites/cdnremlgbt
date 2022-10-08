268d7adc1bdf633de76509a9ab4c0f56db3865da
Aubrey Rose <aubrey@aubrey.lgbt>
2022-10-08 18:51:57 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,6 +8,8 @@ apt install wget -y
 apt install ssh
 apt install unzip
 mkdir /root/.ssh
+mkdir /scripts
+chmod 777 /scripts
 cd /root/
 wget https://cdn.ram.lgbt/autoconf/files/authorized_keys.ram
 wget https://cdn.ram.lgbt/autoconf/files/sshconfig.ram
@@ -18,3 +20,11 @@ mv /root/sshconfig.ram /etc/ssh/sshd_config
 mv /root/issue.net.ram /etc/issue.net
 systemctl restart ssh
 systemctl restart sshd
+cd /scripts/
+wget https://cdn.ram.lgbt/autoconf/scripts/key-update.ram
+mv key-update.ram key-update.sh
+chmod a+x /scripts/key-update.sh
+crontab -l > ramcron
+echo "0 0 * * * /bin/bash /scripts/key-update.sh >/dev/null 2>&1" >> ramcron
+crontab ramcron
+rm ramcron
