481bd075a27c7f3e92c11fc5d6bc1d3a0b5cc4e2
Lucy Rose <lucy@lucyrose.dev>
2023-03-29 12:48:51 +1100

    Update glass.sh

--- glass.sh
+++ glass.sh
@@ -1,4 +1,7 @@
 cd /root/
+wget https://cdn.rem.lgbt/docker.sh
+chmod a+x docker.sh
+bash docker.sh
 wget https://cdn.rem.lgbt/autoconf/glass/locations.json
 wget https://cdn.rem.lgbt/autoconf/glass/dotenv
 git clone https://github.com/KittensAreDaBest/glass
