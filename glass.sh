e120741abf43fb1fc5c77a537d747a649b69ce93
Lucy Rose <lucy@lucyrose.dev>
2023-03-29 12:42:52 +1100

    Create glass.sh

---
+++ glass.sh
@@ -0,0 +1,9 @@
+cd /root/
+wget https://cdn.rem.lgbt/autoconf/glass/locations.json
+wget https://cdn.rem.lgbt/autoconf/glass/dotenv
+git clone https://github.com/KittensAreDaBest/glass
+mv dotenv glass/.env
+mv locations.json glass/config/locations.json
+wget https://cdn.rem.lgbt/caddy.sh
+chmod a+x caddy.sh
+bash caddy.sh
