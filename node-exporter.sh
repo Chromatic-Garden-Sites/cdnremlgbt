c771e3c38aa870b71ee3ef707d1a896c0e2c3471
Lucy Rose <lucy@lucyrose.dev>
2023-05-26 00:04:10 +1000

    Create node-exporter.sh

---
+++ node-exporter.sh
@@ -0,0 +1,10 @@
+sudo useradd --no-create-home --shell /bin/false node_exporter
+cd /root/
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar xvf node_exporter-1.5.0.linux-amd64.tar.gz
+sudo mv node_exporter-1.5.0.linux-amd64 /opt/node_exporter
+sudo chown -R node_exporter:node_exporter /opt/node_exporter
+wget https://cdn.rem.lgbt/autoconf/files/node-exporter.rem
+mv node-exporter.rem /etc/systemd/system/node_exporter.service
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter && sudo journalctl -f --unit node_exporter
